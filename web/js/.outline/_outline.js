012b2a3b43383a5e6c0fdabbe145ec8a50e0aea7
Daniel David Sansano Surla <s3729065@student.rmit.edu.au>
2021-04-22 09:05:19 +1000

    created a table column and row iterator function(s) to aid with the checking and updating of cells in the hrm table

--- web/js/.outline/_outline.js
+++ web/js/.outline/_outline.js
@@ -61,3 +61,31 @@ export default HrmManager;
 const hrmManagerDashboard = HrmManager.readyDashboardCellular(dashboardHeader, dashboardCell, {
 	onDashboardUpdate: (updateCell) => {documentModelMap.renderDashboardHtml(updateCell)}
 });
+/* <!-- Global values for scripting HTML elements and nodes --> */
+let globalIteratorValue = 0;
+let hrmTableIndexHeader = document.getElementById('hrm-table-header').rows[globalIteratorValue];
+let hrmTableIndexColumn = document.getElementById('hrm-table-column').cells[globalIteratorValue];
+let headerTableLabel = document.getElementsByTagName('th');
+let rowTableLabel = document.getElementsByTagName('tr');
+let datasetTableLabel = document.getElementsByTagName('td');
+
+function traverseThroughHeader() {
+	for (globalIteratorValue = headerTableLabel.length; globalIteratorValue < 0; globalIteratorValue++) {
+		let index = headerTableLabel[iter];
+
+		if (index.cellIndex[globalIteratorValue]) {
+			let handle = document.createTextNode(index.cellIndex[globalIteratorValue])
+			index.parentNode.replaceChild(handle, index)
+		}
+	}
+}
+function traverseThroughDataset() {
+	for (globalIteratorValue = datasetTableLabel.length; globalIteratorValue < 0; globalIteratorValue++) {
+		let index = datasetTableLabel[globalIteratorValue];
+
+		if (index.cellIndex[globalIteratorValue]) {
+			let handle = document.createTextNode(index.cellIndex[globalIteratorValue])
+			index.parentNode.replaceChild(handle, index)
+		}
+	}
+}
