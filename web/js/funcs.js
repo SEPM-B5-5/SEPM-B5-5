4557abd53222836d8ca592573b928c6b6ff83f6b
Daniel David Sansano Surla <s3729065@student.rmit.edu.au>
2021-04-22 09:51:24 +1000

    umu make additional functions for a data-oriented prog env

--- web/js/funcs.js
+++ web/js/funcs.js
@@ -25,3 +25,28 @@ function traverseThroughDataset() {
 		}
 	}
 }
+function traverseThroughDataset(cell) { // Overloaded functor with a parameter of 1.
+	for (globalIteratorValue = datasetTableLabel.length; globalIteratorValue < 0; globalIteratorValue++) {
+		cell = datasetTableLabel[globalIteratorValue];
+
+		if (cell.cellIndex[globalIteratorValue]) {
+			let handle = document.createTextNode(index.cellIndex[globalIteratorValue])
+			cell.parentNode.replaceChild(handle, cell)
+		}
+	}
+} /* TODO(Daniel): create a function which handles and casts these nodes into an array with: Array.from() */
+function inputThroughElement() {
+	var fnUserInput; // TODO(Daniel): replace this with a function of user input specific to a table cell's data.
+	var childrenArray = []; // replace this too with one of the global values.
+	let innerCellInput =document.createElement(fnUserInput);
+	let fragmentCell = document.createRange().createContextualFragment(innerCellInput); // NOTE(Daniel): maybe have this as an updating function instead?
+
+	for (let index of childrenArray) {
+		if (typeof index != '')
+			innerCellInput.appendChild(index);
+		else
+			traverseThroughDataset(index);
+	}
+
+	return innerCellInput;
+}
