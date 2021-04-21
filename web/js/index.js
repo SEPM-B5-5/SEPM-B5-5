57832fbe6b358ef379038eb3bf16540015737129
Daniel David Sansano Surla <s3729065@student.rmit.edu.au>
2021-04-21 16:44:35 +1000

    planning out the class and its functions to be accessible thru html dom

--- web/js/index.js
+++ web/js/index.js
@@ -1,19 +1,35 @@
 import 'http' // Import NodeJS files here
 
+class HrmTable {
+	mHrmTable = document.getElementById("hrmTable");
+// TODO(Daniel): index with `data-content` values checked cells with user input.
+	mHrmTableBodyRow = hrmTable.rows;
+	mHrmTableBodyCells = hrmTable.cells;
+
+	constructor(hrmTable) {
+		this.mHrmTable = hrmTable;
+	}
+
+	static fetchHrmTable() {
+		return this.mHrmTable;
+	}
+
+	static fetchRowsRefersHrmTable() {
+		return this.mHrmTableBodyRow;
+	}
+
+	static fetchCellsRefersHrmTable() {
+		return this.mHrmTableBodyCells
+	}
+}
+
 function checkCell() {
 	let index = 0;
-	let cellInRow = 1;
-	let cellInColumn = 1;
 
-	let hrmTable = document.getElementById("hrmTable");
-	let hrmTableBodyRow = hrmTable.insertRow(index);
-	let hrmTableBodyColumn = hrmTable.insertCell(index);
+	if (cellInRow <= fetchHrmTable().length) {
 
-	if (cellInRow == hrmTableBodyRow) {
-		cellInRow++;
-
-		if (cellInColumn == hrmTableBodyColumn)
-			cellInColumn++;
+		/* if (cellInColumn == hrmTableBodyColumn)
+			cellInColumn++; */
 	} else {
 		index++;
 	}
@@ -22,6 +38,17 @@ function checkCell() {
 }
 
 function indexCell() {
-	// TODO(Daniel): index with `data-content` values checked cells with user input.
+	let index = 0;
+
+	if (index <= fetchRowsRefersHrmTable().length) {
+		// TODO
+		if(index <= fetchCellsRefersHrmTable().length) {
+			//TODO
+		}
+	} else {
+		index++;
+	}
+
+	return 0;
 }
 
