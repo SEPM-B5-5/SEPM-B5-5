d1e6f445ec4925f273b1b8070d7425cf0663718d
Daniel David Sansano Surla <s3729065@student.rmit.edu.au>
2021-05-28 23:42:34 +1000

    updated files

--- sepm-b5-5-project/src/main/resources/web/js/NotifsHub.js
+++ sepm-b5-5-project/src/main/resources/web/js/NotifsHub.js
@@ -13,8 +13,8 @@ function renderView() {
 			height = 640 - margin.top - margin.bottom;
 		
 	var svg = d3.select('body').append('svg')
-		.attr('width', width + margin.left + margin.right)
-		.attr('height', height + margin.top + margin.bottom)
+		.attr('width', windowDimensions.width + margin.left + margin.right)
+		.attr('height', windowDimensions.height + margin.top + margin.bottom)
 		.append('g')
 		.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
